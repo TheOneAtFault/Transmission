1982ae253d711a9e78adbb0acc653f718363dc93
Thysie <mjvw.skippy@gmail.com>
2019-11-20 16:24:11 +0200

    Aidl Polling display update

---
+++ aidl/src/main/java/com/ody/aidl/Features/DigitalDisplay.java
@@ -0,0 +1,83 @@
+package com.ody.aidl.Features;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.ody.aidl.Helpers.Response;
+import com.ody.aidl.StartUp;
+import com.ody.aidl.Utils.AidlUtil;
+
+import java.io.File;
+
+public class DigitalDisplay {
+    private static DigitalDisplay mDigtaDigitalDisplay = new DigitalDisplay();
+    private Response response;
+
+    public static DigitalDisplay getInstance() {
+        return mDigtaDigitalDisplay;
+    }
+
+    public Response displayWake() {
+        try {
+            response = AidlUtil.getInstance().lcdWake();
+        } catch (Exception e) {
+            response = Response.getInstance().compose(false, e, "Exception in DigitalDisplay.displayWake()");
+        } finally {
+            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
+        }
+        return response;
+    }
+
+    public Response displaySleep() {
+        try {
+            response = AidlUtil.getInstance().lcdSleep();
+        } catch (Exception e) {
+            response = Response.getInstance().compose(false, e, "Exception in DigitalDisplay.displaySleep()");
+        } finally {
+            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
+        }
+        return response;
+    }
+
+    public Response displayImage(String filePath) {
+        try {
+            //chack file exists
+            File file = new File(filePath);
+            if (file.exists()) {
+                //convert to itty bitty something
+                Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                response = AidlUtil.getInstance().lcdBitmap(bitmap);
+            } else {
+                response = Response.getInstance().compose(false, null, "File does not exist");
+            }
+
+        } catch (Exception e) {
+            response = Response.getInstance().compose(false, e, "Exception in DigitalDisplay.lcdImage");
+        } finally {
+            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
+        }
+        return response;
+    }
+
+    public Response displaySingle(String content) {
+        try {
+            response = AidlUtil.getInstance().lcdSingle(content);
+        } catch (Exception e) {
+            response = Response.getInstance().compose(false, e, "Exception in lcdSingle - DigitalDisplay");
+        } finally {
+            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
+        }
+        return response;
+    }
+
+    public Response displayDouble(String lineOne, String lineTwo) {
+        try {
+            response = AidlUtil.getInstance().lcdDouble(lineOne, lineTwo);
+        } catch (Exception e) {
+            response = Response.getInstance().compose(false, e, "Exception in lcdDouble - DigitalDisplay");
+        } finally {
+            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
+        }
+        return response;
+    }
+}
--- aidl/src/main/java/com/ody/aidl/Services/Display.java
+++ aidl/src/main/java/com/ody/aidl/Services/Display.java
@@ -1,65 +1,52 @@
 package com.ody.aidl.Services;
 
-import android.graphics.Bitmap;
-
+import com.ody.aidl.Features.DigitalDisplay;
 import com.ody.aidl.Helpers.Response;
-import com.ody.aidl.StartUp;
-import com.ody.aidl.Utils.AidlUtil;
 
 public class Display {
     private static Response response;
 
     public static Response wake() {
         try {
-            response = AidlUtil.getInstance().lcdWake();
+            response = DigitalDisplay.getInstance().displayWake();
         } catch (Exception e) {
             response = Response.getInstance().compose(false, e, "Exception in Display.wake()");
-        } finally {
-            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
         }
         return response;
     }
 
     public static Response sleep() {
         try {
-            response = AidlUtil.getInstance().lcdSleep();
+            response = DigitalDisplay.getInstance().displaySleep();
         } catch (Exception e) {
             response = Response.getInstance().compose(false, e, "Exception in Display.sleep()");
-        } finally {
-            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
         }
         return response;
     }
 
-    public static Response lcdBitmap(Bitmap bitmap) {
+    public static Response lcdBitmap(String file) {
         try {
-            response = AidlUtil.getInstance().lcdBitmap(bitmap);
+            response = DigitalDisplay.getInstance().displayImage(file);
         } catch (Exception e) {
             response = Response.getInstance().compose(false, e, "Exception in lcdBitmap");
-        } finally {
-            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
         }
         return response;
     }
 
     public static Response lcdSingle(String content) {
         try {
-            response = AidlUtil.getInstance().lcdSingle(content);
+            response = DigitalDisplay.getInstance().displaySingle(content);
         } catch (Exception e) {
             response = Response.getInstance().compose(false, e, "Exception in lcdSingle - Polling Display");
-        } finally {
-            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
         }
         return response;
     }
 
     public static Response lcdDouble(String lineOne, String lineTwo) {
         try {
-            response = AidlUtil.getInstance().lcdDouble(lineOne, lineTwo);
+            response = DigitalDisplay.getInstance().displayDouble(lineOne, lineTwo);
         } catch (Exception e) {
             response = Response.getInstance().compose(false, e, "Exception in lcdDouble - Polling Display");
-        } finally {
-            AidlUtil.getInstance().disconnectPrinterService(StartUp.getApplication());
         }
         return response;
     }
